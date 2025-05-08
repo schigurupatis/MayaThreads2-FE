1ff3e6870277d02cec904877caf10d1f0e533027
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2025-05-08 23:14:32 +0530

    worked on baseurl design

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
-export const BASE_URL = "http://localhost:7777";
+// export const BASE_URL = "http://localhost:7777";
 // export const BASE_URL = "http://localhost:7777";
 //export const BASE_URL = "/api";
 
 // export const BASE_URL =
 //   location.hostname === "localhost" ? "http://localhost:7777" : "/api";
 
-// export const BASE_URL =
-//   typeof window !== "undefined" && window.location.hostname === "localhost"
-//     ? "http://localhost:3000"
-//     : "/api";
+export const BASE_URL =
+  typeof window !== "undefined" && window.location.hostname === "localhost"
+    ? "http://localhost:7777"
+    : "/api";
